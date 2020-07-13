576494d6243184709aa4c3f41843b4ae942b194b
caozuba <nccuong.hn93@gmail.com>
2020-07-13 22:48:58 +0900

    LoginPage.js can pass data to console log, have input check

--- src/components/content/LoginPage.js
+++ src/components/content/LoginPage.js
@@ -1,36 +1,69 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
-    return(
-        <div className="row">
-            <div className="col-md-8">
-                <div className="card">
-                    <div className="card-header">
-                        <h5 className="title">Edit Profile</h5>
-                    </div>
-                    <div className="card-body">
-                        <form>
-                            <div className="row">
-                                <div className="col-md-6 pr-1">
-                                    <div className="form-group">
-                                        <label>First Name</label>
-                                        <input type="text" className="form-control" placeholder="Company" value="Mike"/>
-                                    </div>
-                                </div>
-                                <div className="col-md-6 pl-1">
-                                    <div className="form-group">
-                                        <label>Last Name</label>
-                                        <input type="text" className="form-control" placeholder="Last Name"
-                                               value="Andrew"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="row">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header">
+            <h5 className="title">Login</h5>
+          </div>
+          <div className="card-body">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="row">
+                <div className="col-md-6 pr-1">
+                  <div className="form-group d-block">
+                    <label>Login ID</label>
+                    <input
+                      type="text"
+                      name="loginId"
+                      ref={register({ required: true })}
+                      className="form-control"
+                      placeholder="Login ID"
+                    />
+                    <p className="text-danger">
+                      {errors.loginId && "Login ID is required"}
+                    </p>
+                  </div>
                 </div>
-            </div>
+                <div className="col-md-6 pl-1">
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      ref={register({ required: true })}
+                      className="form-control"
+                      placeholder="Password"
+                    />
+
+                    <p className="text-danger">
+                      {errors.password && "Password is required"}
+                    </p>
+                  </div>
+                </div>
+                <div className="col-md-6 pl-1 align-content-center">
+                  <div className="form-group">
+                    <input
+                      type="submit"
+                      className="btn btn-primary"
+                      value="Login"
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
